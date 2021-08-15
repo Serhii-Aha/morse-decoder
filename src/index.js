54650b856ea381edfd5ac6f9b667dc17491006e6
unknown <a1002android@gmail.com>
2021-08-15 17:03:02 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let char = ''; char_expr = ''; word = '';
+    let end_expr = expr.length;
+    for (let i = 0; i < end_expr; i += 10) {
+        if (expr[i] != '*') {
+            char_expr = expr.substr(i, 10);      
+            char_expr = char_expr.substr(char_expr.indexOf("1"));
+            let char_morse = '';
+            for (let j = 0; j < char_expr.length; j += 2) {
+                if ((char_expr[j] + char_expr[j + 1]) == '10') {
+                    char_morse += '.'
+                } else {
+                    char_morse += '-'
+                }
+            }
+            char = MORSE_TABLE[char_morse];
+        } else {
+            char = ' ';
+        }
+        word += char;
+    }
+    return word;
 }
 
 module.exports = {
